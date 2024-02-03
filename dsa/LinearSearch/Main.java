6ff6004531805c7d0881f46514180cd1170f049a
Aditya Gohil <89626455+Adi0015@users.noreply.github.com>
2024-02-03 06:15:06 +0530

    Main

--- dsa/LinearSearch/Main.java
+++ dsa/LinearSearch/Main.java
@@ -1,12 +1,47 @@
 package myJavaPractice.dsa.LinearSearch;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    int[] nums = {24,56,89,45,12,9,92,-2,-4};
-    int target = -2 ;
-    System.out.println(linearSearch(nums, target));
+    Scanner in = new Scanner(System.in);
+    int[] nums = { 24, 56, 89, 45, 12, 9, 92, -2, -4 };
+    System.out.println("Enter the Target num");
+    int target = in.nextInt();
+    System.out.println(linearSearch3(nums, target));
+    in.close();
   }
 
+  // ! Linear Search and returning element
+  static int linearSearch2(int[] arr, int target) {
+    if (arr.length == 0) {
+      return -1;
+    }
+    for (int element : arr) {
+        if (element == target){
+
+          System.out.println("got it");
+          return element;
+        }
+      
+    }
+    return Integer.MAX_VALUE;
+  }
+  // ! Linear Search for Boolean output
+  static boolean linearSearch3(int[] arr, int target) {
+    if (arr.length == 0) {
+      return false;
+    }
+    for (int element : arr) {
+        if (element == target){
+
+          System.out.println("got it");
+          return true;
+        }
+      
+    }
+    return false;
+  }
+    //! Linear Search returning index
   static int linearSearch(int[] arr, int target) {
     if (arr.length == 0) {
       return -1;
@@ -15,7 +50,7 @@ public class Main {
       int element = arr[i];
       if (element == target) {
         System.out.println("got it");
-        return i ;
+        return i;
       }
     }
     return -1;

19bff28c4c205e95c5c3c1ad9de7ebaf4d7af592
Aditya Gohil <89626455+Adi0015@users.noreply.github.com>
2024-02-11 09:14:24 +0530

    2D_BinarySearch

---
+++ dsa/Binary_2dSearch/RowCol.java
@@ -0,0 +1,24 @@
+package dsa.Binary_2dSearch;
+
+import java.util.Arrays;
+
+public class RowCol {
+    public static void main(String[] args) {
+    int[][] arr = { {10,20,30,40},
+                    {15,25,35,45},
+                    {28,29,37,49},
+                    {33,34,38,50} };  
+    System.out.println(Arrays.toString(search(arr, 29)));
+    }
+    static int[] search(int[][] matrix ,int target ){
+      int r = 0;
+      int c = matrix.length -1 ;
+      while(r < matrix.length && c>=0){
+        if (matrix[r][c] == target){return new int[]{r,c};} 
+        if (matrix[r][c] > target){c--;}
+        // if (matrix[r][c] < target){r++;}
+        else{r++;}
+      }
+      return new int[]{-1,-1};
+    }
+}
